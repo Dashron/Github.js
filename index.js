5d3e0f84105bf4b3d691dad14e2b5fcc41f5fbb2
Aaron Hedges <aaron@dashron.com>
2013-03-01 14:57:34 -0500

    add a way to override uris

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 module.exports.Github = require('./libs/github').Github;
+
+/**
+ * should be provided an object. The default is {
+ * 	host : 'api.github.com',
+ * 	authorization : 'https://github.com/login/oauth/authorize',
+ * 	access : 'https://github.com/login/oauth/access_token'
+ * }
+ * @param {[type]} uris [description]
+ */
+module.exports.setUris = function (uris) {
+	require('./libs/github').uris = uris;
+}
